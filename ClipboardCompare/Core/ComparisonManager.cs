13f4bc523f620b94e79a23f17bdc9d92478d2e07
jchindev <jchin1000@gmail.com>
2016-09-15 14:19:56 -0700

    Initial commit

---
+++ ClipboardCompare/Core/ComparisonManager.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace ClipboardCompare.Core
+{
+    public class ComparisonManager
+    {
+        private readonly string _comparisonToolPath = ConfigurationManager.AppSettings["CompareToolPath"];
+        private readonly string _comparisonToolArguments = ConfigurationManager.AppSettings["CompareToolArgs"];
+
+        private string _clip1Text;
+        private string _clip2Text;
+
+        public string Clip1Text
+        {
+            get { return _clip1Text; }
+            set { _clip1Text = value; }
+        }
+
+        public string Clip2Text
+        {
+            get { return _clip2Text; }
+            set { _clip2Text = value; }
+        }
+
+        public string[] CreateTempFiles()
+        {
+            string workDirPath = GetWorkingDirectoryPath();
+
+            DirectoryInfo di = new DirectoryInfo(workDirPath);
+            if (!di.Exists)
+            {
+                di.Create();
+            }
+
+            var selectionFile = workDirPath + Path.DirectorySeparatorChar + "clip_" + Guid.NewGuid() + ".txt";
+            File.WriteAllText(selectionFile, _clip1Text);
+
+            var clipboardFile = workDirPath + Path.DirectorySeparatorChar + "clip_" + Guid.NewGuid() + ".txt";
+            File.WriteAllText(clipboardFile, _clip2Text);
+
+            return new[] { selectionFile, clipboardFile };
+        }
+
+        public void ExecuteCleanup()
+        {
+            string workDirPath = GetWorkingDirectoryPath();
+
+            DirectoryInfo di = new DirectoryInfo(workDirPath);
+            if (di.Exists)
+            {
+                var fileInfos = di.GetFiles("clip_*").ToList();
+                fileInfos.ForEach(fi =>
+                {
+                    fi.Delete();
+                });
+            }
+        }
+
+        private string GetWorkingDirectoryPath()
+        {
+            string workDirPath = "../../Temp";
+            return workDirPath;
+        }
+
+        public void OpenComparisonProcess()
+        {
+            try
+            {
+                ExecuteCleanup();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            string[] tempFiles = null;
+            try
+            {
+                tempFiles = CreateTempFiles();
+
+                System.Diagnostics.Process proc = new System.Diagnostics.Process
+                {
+                    EnableRaisingEvents = false,
+                    StartInfo =
+                    {
+                        FileName = _comparisonToolPath,
+                        Arguments = $"{_comparisonToolArguments} {tempFiles[0]} {tempFiles[1]}"
+                    }
+                };
+
+                if (File.Exists(tempFiles[0]) && File.Exists(tempFiles[1]) ||
+                    Directory.Exists(tempFiles[0]) && Directory.Exists(tempFiles[1]))
+                {
+                    proc.Start();
+                }
+                else
+                {
+                    MessageBox.Show("Could not find files to compare!");
+                }             
+            }
+            catch (Exception e)
+            {
+                if (tempFiles != null)
+                {
+                    File.Delete(tempFiles[0]);
+                    File.Delete(tempFiles[1]);
+                }
+
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+}
--- ClipboardCompare/MainWindow.xaml.cs
+++ ClipboardCompare/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ClipboardCompare.Core;
 
 namespace ClipboardCompare
 {
@@ -20,9 +21,37 @@ namespace ClipboardCompare
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly ComparisonManager _comparisonManager;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _comparisonManager = new ComparisonManager();
+        }
+
+        private void btnClip1_Click(object sender, RoutedEventArgs e)
+        {
+            _comparisonManager.Clip1Text = Clipboard.GetText();
+            imgClip1.Visibility = string.IsNullOrEmpty(_comparisonManager.Clip1Text) ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        private void btnClip2_Click(object sender, RoutedEventArgs e)
+        {
+            _comparisonManager.Clip2Text = Clipboard.GetText();
+            imgClip2.Visibility = string.IsNullOrEmpty(_comparisonManager.Clip2Text) ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        private void btnSwap_Click(object sender, RoutedEventArgs e)
+        {
+            var tempText = _comparisonManager.Clip1Text;
+            _comparisonManager.Clip1Text = _comparisonManager.Clip2Text;
+            _comparisonManager.Clip2Text = tempText;
+        }
+
+        private void btnCompare_Click(object sender, RoutedEventArgs e)
+        {
+            _comparisonManager.OpenComparisonProcess();
         }
     }
 }
